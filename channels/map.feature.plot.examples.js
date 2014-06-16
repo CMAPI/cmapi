4b947c80e349cbc123130ea54c85fd00bb0ed7da
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-06-16 11:48:57 -0700

    updates to plot examples for failure case to have valid marked as false

--- channels/map.feature.plot.examples.js
+++ channels/map.feature.plot.examples.js
@@ -12,7 +12,7 @@ cmapi.channel["map.feature.plot"].examples = [
     },
     {
     "title": "Plot a single KML",
-    "valid": true,
+    "valid": false,
     "payload": {
         "overlayId": "2d882141-0d9e-59d4-20bb-58e6d0460699.1", 
         "feature": "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\"><Placemark id=\"example.mapWidget.1.1\"><name>World Trade Center</name><description><![CDATA[Site of World Trade Center]]></description><Style><IconStyle><Icon><href>https://localhost/widgets/mapWidget/images/blu-circle.png</href></Icon><hotSpot x=\"0.5\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"></hotSpot></IconStyle></Style><Point><coordinates>-74.01324033737183,40.71149172571141,0 </coordinates></Point></Placemark></kml>", 
