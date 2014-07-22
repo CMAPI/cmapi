93fbf6d843f04121193b8124f8413244e18b4123
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-07-22 13:50:58 -0700

    Work in progress updating table of contents

--- channels/cmapi.overview.js
+++ channels/cmapi.overview.js
@@ -48,6 +48,13 @@ cmapi.overview["cmapi.overview"] = {
 		"paragraphs": [
 			"All Payloads can be either a single object or an array of like objects."
 		]
+	},
+	{
+		"title": "Batch Behavior",
+		"paragraphs": [
+			"Batch processing allows an application to send a group of messages bundled with top level meta data.  This enables reduction of duplicate values, such as overlayId on a map.feature.plot, where 500 features are targeting the overlay.  This also allows the sending application to treat the entire batch as one message and leverage the map.message.complete to know when this batch of messages has finished processing, as well as seeing any errors raised during the processing.",
+			"In previous versions of CMAPI (1.2.0 and earlier) a message payload was allowed to be either an object representing a single payload, or an array representing multiple payloads.  Beginning in CMAPI 1.2.1 developers SHOULD use the batch channels in place of arrays of payloads."
+		]
 	}, {
 		"title": "Latitude and Longitude",
 		"paragraphs": [
