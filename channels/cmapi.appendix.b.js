78753e95d73941ee788731f3c420caf55475bf3e
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-07-22 14:12:49 -0700

    Minor formatting tweaks Removed required: [ "type"] in cmapi.appendix.b Changed required:  ["type"]  in cmapi.appendix.c to "aoi" Changed <br> to self closing <br/> in cmapi.appendix.c

--- channels/cmapi.appendix.b.js
+++ channels/cmapi.appendix.b.js
@@ -5,155 +5,154 @@ cmapi.channel["cmapi.appendix.b"] = {
     "title": "Appendix B - GeoJSON Support",
     "description": "The GeoJSON specification can be found at <a href=\"http://geojson.org/geojson-spec.html\" >http://geojson.org/geojson-spec.html</a>.  The Common Map Widget API specification extends the GeoJSON specification by adding the “style”, “name”, “id”, “description“, and “timePrimitive“ objects to the “Properties” object of the GeoJSON specification.  These extended objects ONLY apply to the GeoJSON Feature object.",
     "type": "object",
-    "required": ["type"],
+    "properties": {
+      "name": {
+        "type": "string",
+        "description": "name of the specific GeoJSON feature.  Generally used when the GeoJSON parent object is a featureCollection or feature objects."
+      },
+      "id": {
+        "type": "string",
+        "description": "a unique identifier for the feature object.  Though not required, it is RECOMMENDED. If the id of the GeoJSON Feature.properties.id is omitted, and part of a FeatureCollection, selection may not work for these features as they cannot be uniquely identified. "
+      },
+      "description": {
+        "type": "string",
+        "description": "user supplied content that appears in a pop-up balloon associated with the feature.  Can be plain text, or HTML formatted."
+      },
+      "timePrimitive": {
+        "type": "object",
+        "description": "",
         "properties": {
-          "style": {
+          "timeSpan": {
             "type": "object",
             "description": "",
             "properties": {
-              "lineStyle": {
-                "type": "object",
-                "description": "",
-                "properties": {
-                  "color": {
-                    "description": "Object representing CSS3 RGBA.  See Note 1 below for more info",
-                    "type": "object",
-					"default": "No value sent results in default settings on the map.",
-                    "properties": {
-                      "r": {
-                        "description": "Integer value between 0 and 255 for red.",
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 255
-                      },
-                      "g": {
-                        "description": "Integer value between 0 and 255 for green.",
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 255
-                      },
-                      "b": {
-                        "description": "Integer value between 0 and 255 for green.",
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 255
-                      },
-                      "a": {
-                        "description": "Number value between 0.0 (fully transparent) to 1.0 (fully opaque).",
-                        "type": "number",
-                        "minimum": 0,
-                        "maximum": 1
-                      }
-                    },
-                    "required": [
-                      "r",
-                      "g",
-                      "b",
-                      "a"
-                    ]
-                  }
-                },
-				"required": ["color"]
-              },
-              "polyStyle": {
-                "type": "object",
-                "description": "",
-				"properties": {
-                  "color": {
-                    "description": "Object representing CSS3 RGBA.  See Note 1 below for more info. ",
-                    "type": "object",
-					"default": "No value sent results in default settings on the map.",
-                    "properties": {
-                      "r": {
-                        "description": "Integer value between 0 and 255 for red.",
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 255
-                      },
-                      "g": {
-                        "description": "Integer value between 0 and 255 for green.",
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 255
-                      },
-                      "b": {
-                        "description": "Integer value between 0 and 255 for blue.",
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 255
-                      },
-                      "a": {
-                        "description": "Number value between 0.0 (fully transparent) to 1.0 (fully opaque).",
-                        "type": "number",
-                        "minimum": 0,
-                        "maximum": 1
-                      }
-                    },
-                    "required": [
-                      "r",
-                      "g",
-                      "b",
-                      "a"
-                    ]
-                  }
-                },
-				"required": ["color"]
-              },
-              "iconStyle": {
-                "type": "object",
-                "description": "",
-                "properties": {
-                  "url": {
-                    "type": "uri",
-                    "description": "URL to an image file that will be used for the icon for a point.",
-					"default": "If no URL is provided, result will be map’s default icon."
-                  }
-                },
-				"required": ["url"]
-              },
-              "name": {
+              "begin": {
                 "type": "string",
-                "description": "name of the specific GeoJSON feature.  Generally used when the GeoJSON parent object is a featureCollection or feature objects."
+                "format": "date-time",
+                "description": "time stamp identifying the beginning of the time span (see timeStamp definition for format info). "
               },
-              "id": {
+              "end": {
                 "type": "string",
-                "description": "a unique identifier for the feature object.  Though not required, it is RECOMMENDED. If the id of the GeoJSON Feature.properties.id is omitted, and part of a FeatureCollection, selection may not work for these features as they cannot be uniquely identified. "
-              },
-			  "description": {
-                "type": "string",
-                "description": "user supplied content that appears in a pop-up balloon associated with the feature.  Can be plain text, or HTML formatted."
-              },
-              "timePrimitive": {
-                "type": "object",
-				"description": "",
-                "properties": {
-                  "timeSpan": {
-                    "type": "object",
-					"description": "",
-                    "properties": {
-                      "begin": {
-                        "type": "string",
-                        "format": "date-time",
-                        "description": "time stamp identifying the beginning of the time span (see timeStamp definition for format info). "
-                      },
-                      "end": {
-                        "type": "string",
-                        "format": "date-time",
-                        "description": "time stamp identifying the end of the time span (see timeStamp definition for format info)."
-                      }
-                    },
-					"required": ["begin", "end"]
-                  },
-                  "timeStamp": {
-                    "type": "string",
-                    "format": "date-time",
-                    "description": "time stamp value expressed as yyyy-mm-ddThh:mm:ss.ssszzzzzz, where T is the separator between the date and the time, and the time zone is either Z (for UTC) or zzzzzz, which represents ±hh:mm in relation to UTC. Additionally, the value can be expressed as a date only."
-                  }
-                }
+                "format": "date-time",
+                "description": "time stamp identifying the end of the time span (see timeStamp definition for format info)."
               }
-            }
+            },
+            "required": ["begin", "end"]
+          },
+          "timeStamp": {
+            "type": "string",
+            "format": "date-time",
+            "description": "time stamp value expressed as yyyy-mm-ddThh:mm:ss.ssszzzzzz, where T is the separator between the date and the time, and the time zone is either Z (for UTC) or zzzzzz, which represents ±hh:mm in relation to UTC. Additionally, the value can be expressed as a date only."
           }
+        }
+      },
+      "style": {
+        "type": "object",
+        "description": "",
+        "properties": {
+          "lineStyle": {
+            "type": "object",
+            "description": "",
+            "properties": {
+              "color": {
+                "description": "Object representing CSS3 RGBA.  See Note 1 below for more info",
+                "type": "object",
+                "default": "No value sent results in default settings on the map.",
+                "properties": {
+                  "r": {
+                    "description": "Integer value between 0 and 255 for red.",
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 255
+                  },
+                  "g": {
+                    "description": "Integer value between 0 and 255 for green.",
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 255
+                  },
+                  "b": {
+                    "description": "Integer value between 0 and 255 for green.",
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 255
+                  },
+                  "a": {
+                    "description": "Number value between 0.0 (fully transparent) to 1.0 (fully opaque).",
+                    "type": "number",
+                    "minimum": 0,
+                    "maximum": 1
+                  }
+                },
+                "required": [
+                  "r",
+                  "g",
+                  "b",
+                  "a"
+                ]
+              }
+            },
+            "required": ["color"]
+          },
+          "polyStyle": {
+            "type": "object",
+            "description": "",
+            "properties": {
+              "color": {
+                "description": "Object representing CSS3 RGBA.  See Note 1 below for more info. ",
+                "type": "object",
+                "default": "No value sent results in default settings on the map.",
+                "properties": {
+                  "r": {
+                    "description": "Integer value between 0 and 255 for red.",
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 255
+                  },
+                  "g": {
+                    "description": "Integer value between 0 and 255 for green.",
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 255
+                  },
+                  "b": {
+                    "description": "Integer value between 0 and 255 for blue.",
+                    "type": "integer",
+                    "minimum": 0,
+                    "maximum": 255
+                  },
+                  "a": {
+                    "description": "Number value between 0.0 (fully transparent) to 1.0 (fully opaque).",
+                    "type": "number",
+                    "minimum": 0,
+                    "maximum": 1
+                  }
+                },
+                "required": [
+                  "r",
+                  "g",
+                  "b",
+                  "a"
+                ]
+              }
+            },
+            "required": ["color"]
+          },
+          "iconStyle": {
+            "type": "object",
+            "description": "",
+            "properties": {
+              "url": {
+                "type": "uri",
+                "description": "URL to an image file that will be used for the icon for a point.",
+                "default": "If no URL is provided, result will be map’s default icon."
+              }
+            },
+            "required": ["url"]
+          }
+        }
       }
+    }
   },
   notes: [
     'See <a href=\"http://www.w3.org/wiki/CSS3/Color/RGBA" >http://www.w3.org/wiki/CSS3/Color/RGBA</a> for more info on RGBA.'
