1af363dc9313c878b2c19bac7f3cd735150172d5
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-08-28 03:39:55 -0700

    Added property extension info

--- channels/map.overlay.hide.js
+++ channels/map.overlay.hide.js
@@ -12,11 +12,15 @@ cmapi.channel["map.overlay.hide"] = {
       "messageId": {
         "description": "A globally unique ID that identifies a particular message.  This ID SHALL be used for the lifetime of the message and is used to identify map.message.progress and map.message.complete messages that correlate to the original message with the same ID.  When sending a messageId a map that supports the user manipulation extension SHALL send map.message.progress and map.message.complete messages where appropriate.  See the map.message channels under the User Manipulation extension for more information.",
         "type": "string",
-        "status": "new"
+        "status": "new",
+        "extension": "User Manipulation - Message Complete"
       },
     },
     "required": []
   },
   "notes": [],
-  "changeLog": [{"version": "1.3.0", "change" : "Added optional messageId parameter to support user manipulation extension"}]
+  "changeLog": [{
+    "version": "1.3.0",
+    "change": "Added optional messageId parameter to support user manipulation extension"
+  }]
 };
