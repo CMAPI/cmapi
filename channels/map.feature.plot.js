f192a0c7982a3f7d98ac1cb892552b6a586ade65
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-08-27 20:59:05 -0700

    Minor formatting changes

--- channels/map.feature.plot.js
+++ channels/map.feature.plot.js
@@ -8,17 +8,17 @@ cmapi.channel["map.feature.plot"] = {
       "overlayId": {
         "description": "The ID of the overlay this feature should be loaded into. If an overlay with this ID already exists, the new feature is merged into existing overlay; otherwise, a new overlay is created. If no overlayId is included, default overlay with ID equal to sending widget’s ID is used. If an overlay exists, it will retain its status (whether visible or hidden). If an overlay is created, it will be made visible.",
         "type": "string",
-		"default": "sending widget's ID"
+        "default": "sending widget's ID"
       },
       "featureId": {
         "description": "Unique identifier for the given feature data. Note that feature IDs MUST be unique within a given overlay. Reusing a feature ID will be considered a reload, with the original feature data being removed and replaced by the new feature data.",
         "type": "string",
-		"default": "N/A"
+        "default": "N/A"
       },
       "name": {
         "description": "Name for the given feature data. Note that feature names do not have to be unique and are intended for display purposes only.",
         "type": "string",
-		"default": "N/A"
+        "default": "N/A"
       },
       "format": {
         "description": "Data format of the given feature. All map implementations MUST support “kml” and “geojson”.  If no format is specified, the format defaults to “kml.” A list of formats supported by a particular map implementation may be obtained by querying the map using the map.status channel (see map.status).",
@@ -31,14 +31,14 @@ cmapi.channel["map.feature.plot"] = {
           "object",
           "string"
         ],
-		"default": "N/A"
+        "default": "N/A"
       },
       "zoom": {
         "description": "true if map should zoom to newly loaded feature data, false if not. Default is false.",
         "type": "boolean",
         "default": false
       },
-	   "readOnly": {
+      "readOnly": {
         "description": "Valid values are “true” or “false”. If “true”, then the end user MUST NOT be able to edit the feature from the map’s user interface, if “false” the end user MAY edit the feature from the map’s user interface. Default value is “true”.   If an edit takes place, the map SHOULD dispatch a map.feature.plot with the updated feature to ensure other widgets are aware that a change took place.",
         "type": "boolean",
         "default": true
