0521d97686c7d68de68c8890088c39b457f74416
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-08-28 03:37:50 -0700

    Added new channel - map.menu.remove

---
+++ channels/map.menu.remove.js
@@ -0,0 +1,24 @@
+cmapi.channel["map.menu.remove"] = {
+  "schema": {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "map.menu.remove",
+    "description": "Removes a context menu and the child items.  ",
+    "properties": {
+      "menuId": {
+        "description": "The unique ID of the menu.  This ID can be used to associate a menu with a feature in a map.feature.plot message.",
+        "type": "string"
+      },
+      "messageId": {
+        "description": "A globally unique ID that identifies a particular message.  This ID SHALL be used for the lifetime of the message and is used to identify map.message.progress and map.message.complete messages that correlate to the original message with the same ID.  When sending a messageId a map that supports the user manipulation extension SHALL send map.message.progress and map.message.complete messages where appropriate.  See the map.message channels under the User Manipulation extension for more information.",
+        "type": "string"
+      }
+    },
+    "required": ["menuId"]
+  },
+  "notes": [],
+  "changeLog": [{
+    "version": "1.3.0",
+    "change": "This channel is new"
+  }]
+
+};
