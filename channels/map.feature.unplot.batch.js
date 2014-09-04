a81ad3eac26c40ddf22bcf0af0c46b0cc9c0dc27
Ismael M Rivera <im.rivera@comcast.net>
2014-09-04 14:30:43 -0400

    Made format changes.

--- channels/map.feature.unplot.batch.js
+++ channels/map.feature.unplot.batch.js
@@ -6,11 +6,11 @@ cmapi.channel["map.feature.unplot.batch"] = {
     "type": "object",
     "properties": {
       "payloads": {
-        "description" : "An array of map.feature.unplot payloads.  See map.feature.unplot for the object format and schema",
+        "description" : "An array of map.feature.unplot payloads minus the messageId.  See map.feature.unplot for the object format and schema",
         "type": "Array"
       },
       "overlayId": {
-        "description": "when included at the array level, this value will be applied to all map.feature.unplot objects in the features array that don’t include an overlayID. I.e., similar behavior to CSS.  See map.feature.unplot for definition of this property",
+        "description": "when included at the array level, this value will be applied to all map.feature.unplot objects in the features array that don't include an overlayID. I.e., similar behavior to CSS.  See map.feature.unplot for definition of this property",
         "type": "string",
         "default": ""
       },
