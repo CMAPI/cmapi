bcbeebab2f40164def29ff5398dfffbb2ca0a953
Ismael M Rivera <im.rivera@comcast.net>
2014-09-04 06:53:37 -0400

    Dropped extended characters.

--- channels/map.feature.deselected.batch.js
+++ channels/map.feature.deselected.batch.js
@@ -2,7 +2,7 @@ cmapi.channel["map.feature.deselected.batch"] = {
   schema: {
     "$schema": "http://json-schema.org/draft-04/schema#",
     "title": "map.feature.deselected.batch",
-    "description": "“De-Select,” or report that a collection of feature objects were de-selected.",
+    "description": "De-Select, or report that a collection of feature objects were de-selected.",
     "type": "object",
     "properties": {
       "payloads": {
@@ -10,7 +10,7 @@ cmapi.channel["map.feature.deselected.batch"] = {
         "type": "Array"
       },
       "overlayId": {
-        "description": "The default overlayId to be applied to all map.feature.deSelected objects in the payloads array that don’t include an overlayId. I.e., similar behavior to CSS.  See map.feature.deSelected for more details",
+        "description": "The default overlayId to be applied to all map.feature.deSelected objects in the payloads array that don't include an overlayId. I.e., similar behavior to CSS.  See map.feature.deSelected for more details",
         "type": "string",
         "default": ""
       },
