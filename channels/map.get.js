d4cdaa9f1c49f15bfa8d9506e7b3f92f56fa8640
Ismael M Rivera <im.rivera@comcast.net>
2014-09-04 15:12:15 -0400

    Redefined the filter property

--- channels/map.get.js
+++ channels/map.get.js
@@ -15,11 +15,21 @@ cmapi.channel["map.get"] = {
       },
       "filter" : {
         "type" : "array",
+        "items": {
+            "type": "object",
+            "properties":{
+                "property":{
+                    "type": "string",
+                    "description": "This field must contain the name of the property field which value must match the value inthe term property."
+                },
+                "term":{
+                    "type": ["string", "number", "boolean"],
+                    "description": "This field must contain the value to match. Type checking is performed. ('10' !== 10)"
+                }
+            }
+        },
+        "additionalItems": true,
         "description" : "An array of terms to search for on the overlays and features."
-      },
-      "select" : {
-        "type" : "array",
-        "description" : ""
       },
       "messageId": {
         "description": "A globally unique ID that identifies a particular message.  This ID SHALL be used for the lifetime of the message and is used to identify map.message.progress and map.message.complete messages that correlate to the original message with the same ID.  When sending a messageId a map that supports the user manipulation extension SHALL send map.message.progress and map.message.complete messages where appropriate.  See the map.message channels under the User Manipulation extension for more information.",
