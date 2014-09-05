644f16252a118ecced256953e512093b356215e9
Ismael M Rivera <im.rivera@comcast.net>
2014-09-05 08:27:08 -0400

    Updated the description.

--- channels/map.status.request.js
+++ channels/map.status.request.js
@@ -6,7 +6,7 @@ cmapi.channel["map.status.request"] = {
     "type": "object",
     "properties": {
       "types": {
-        "description": "An array of status types being requested. Map widgets MUST support status types of “about,” “format,”, “selected”, “view”, <span class=\"updatedContent\">and “initialization” </span>by responding with the applicable status message (e.g., map.status.about, map.status.format, map.status.selected and/or map.status.view). If the types attribute is not included, all status messages MUST be generated.",
+        "description": "An array of status types being requested. Map widgets MUST support status types of about, format, selected, view, <span class=\"updatedContent\">and initialization </span>by responding with the applicable status message (e.g., map.status.about, map.status.format, map.status.selected and/or map.status.view). If the types attribute is not included, all status messages MUST be generated.",
         "type": "array",
         "default": "N/A",
         "uniqueItems": true,
@@ -18,6 +18,6 @@ cmapi.channel["map.status.request"] = {
   "notes": [],
   "changeLog": [{
     "version": "1.3.0",
-    "change": "Added “initialization” as a request option"
+    "change": "Added initialization as a request option"
   }]
 };
