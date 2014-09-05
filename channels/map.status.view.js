701cfc2d8819708831d9dbf1d676c40ec3c8dd47
Ismael M Rivera <im.rivera@comcast.net>
2014-09-05 09:04:00 -0400

    Dropped requestor field and added the messageId.

--- channels/map.status.view.js
+++ channels/map.status.view.js
@@ -5,82 +5,82 @@ cmapi.channel["map.status.view"] = {
     "description": "Send out the current status of the map view",
     "type": "object",
     "properties": {
-      "requestor": {
-        "type": "string",
-		"default": " ",
-        "description": "Client that requested this status message be sent (if any). If no requester, message is being sent due to a map view change."
-      },
-       "bounds": {
-        "description": "Bounding box of area visible on map.",
-        "type": "object",
-		"default": " ",
-        "properties": {
-          "southWest": {
-            "description": "Bottom right of the bounds",
+        "bounds": {
+            "description": "Bounding box of area visible on map.",
             "type": "object",
+            "default": " ",
             "properties": {
-              "lat": {
-                "type": "number",
-                "description": "The latitude value of the point",
-                "minimum": "-90",
-                "maximum": "90"
-              },
-              "lon": {
-                "type": "number",
-                "description": "The longitude value of the point",
-                "minimum": "-180",
-                "maximum": "180"
-              }
+                "southWest": {
+                    "description": "Bottom right of the bounds",
+                    "type": "object",
+                    "properties": {
+                        "lat": {
+                            "type": "number",
+                            "description": "The latitude value of the point",
+                            "minimum": "-90",
+                            "maximum": "90"
+                        },
+                        "lon": {
+                            "type": "number",
+                            "description": "The longitude value of the point",
+                            "minimum": "-180",
+                            "maximum": "180"
+                        }
+                    },
+                    "required": ["lat", "lon"]
+                },
+                "northEast": {
+                    "description": "Top left of the bounds",
+                    "type": "object",
+                    "properties": {
+                        "lat": {
+                            "type": "number",
+                            "description": "The latitude value of the point",
+                            "minimum": "-90",
+                            "maximum": "90"
+                        },
+                        "lon": {
+                            "type": "number",
+                            "description": "The longitude value of the point",
+                            "minimum": "-180",
+                            "maximum": "180"
+                        }
+                    },
+                    "required": ["lat", "lon"]
+                }
+            },
+            "required": ["southWest", "northEast"]
+        },
+        "center": {
+            "type": "object",
+            "default": " ",
+            "description": "The current center of the map",
+            "properties": {
+                "lat": {
+                    "type": "number",
+                    "description": "The latitude of the location that was clicked",
+                    "minimum": "-90",
+                    "maximum": "90"
+                },
+                "lon": {
+                    "type": "number",
+                    "description": "The longitude of the location that was clicked",
+                    "minimum": "-180",
+                    "maximum": "180"
+                }
             },
             "required": ["lat", "lon"]
-          },
-          "northEast": {
-            "description": "Top left of the bounds",
-            "type": "object",
-            "properties": {
-              "lat": {
-                "type": "number",
-                "description": "The latitude value of the point",
-                "minimum": "-90",
-                "maximum": "90"
-              },
-              "lon": {
-                "type": "number",
-                "description": "The longitude value of the point",
-                "minimum": "-180",
-                "maximum": "180"
-              }
-            },
-			"required": ["lat", "lon"]
-          }
-        },
-		"required": ["southWest", "northEast"]
-      },
-	  "center": {
-        "type": "object",
-		"default": " ",
-        "description": "The current center of the map",
-        "properties": {
-          "lat": {
+        },   
+        "range": {
+            "description": "The current distance, in meters, map is zoomed out",
             "type": "number",
-            "description": "The latitude of the location that was clicked",
-            "minimum": "-90",
-            "maximum": "90"
-          },
-          "lon": {
-            "type": "number",
-            "description": "The longitude of the location that was clicked",
-            "minimum": "-180",
-            "maximum": "180"
-          }
+            "default": " "
         },
-		"required": ["lat", "lon"]
-      },   
-      "range": {
-        "description": "The current distance, in meters, map is zoomed out",
-        "type": ["number"],
-		"default": " "
-      }
+        "messageId": {
+          "description": "This ID that should match the ID provided in the map.status.request message.",
+          "type": "string",
+          "status": "new"
+        }
     },
     "required": ["bounds", "center", "range"]
   },
