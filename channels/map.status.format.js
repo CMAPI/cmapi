12a5497774da3eff836ede7a3010310e4e3a8b5d
Ismael M Rivera <im.rivera@comcast.net>
2014-09-05 08:07:37 -0400

    Updated the definition of the format property.

--- channels/map.status.format.js
+++ channels/map.status.format.js
@@ -5,15 +5,33 @@ cmapi.channel["map.status.format"] = {
     "description": "Send out the list of data formats that the map widget supports; in other words, this map implementation supports the following feature data formats.",
     "type": "object",
     "properties": {
-      "formats": {
-        "description": "An array of the formats that this map supports. Note that for this version of the Common Map Widget API, all map implementations MUST support 'kml', 'geojson' and 'wms'.  Additional map formats MAY be supported.",
-        "type": "array",
-        "uniqueItems": true,
-        "default": ["kml"],
-        "items": {
-          "anyOf": ["kml", "geojson", "wms"]
-        }
-      }
+        "formats":{
+            "type":"array",
+            "items":{
+                "type":"object",
+                "properties":{
+                    "format":{
+                        "type": "string",
+                        "description": "This contans the format supported."
+                    },
+                    "supportedOperations":{
+                        "type": "array",
+                        "items":{
+                            "properties":{
+                                "enum": ["plot", "draw", "edit"],
+                                "description": "<ol><li>plot - The map engine supports ploting an object of this format.</li><li>draw - The map engine supports drawing an obect of this format.</li><li>edit - The map engine supports editing objects of theis format.</li></ol>"
+                            }
+                        },
+                        "uniqueItems": true,
+                        "minItems": 1,
+                        "maxItems": 3
+                    }
+                }
+            },
+            "uniqueItems": true,
+            "minItems": 1,
+            "description": "An array of formats supported by the current map engine implementation."
+        },
     },
     "required": ["formats"]
   },
