2209bb0b3e7f10ec70091be1547de776d179b16c
Ismael M Rivera <im.rivera@comcast.net>
2014-09-05 09:23:55 -0400

    Formating corrections.

--- channels/map.view.zoom.js
+++ channels/map.view.zoom.js
@@ -5,15 +5,13 @@ cmapi.channel["map.view.zoom"] = {
         "description": "Zoom the map to a particular range",
         "type": "object",
         "properties": {
-                  "range": {
-                    "description": "The distance in meters from the map (note that most 2-D map renderers do not support infinite zoom and the range will be translated into the nearest supported zoom level).",
-                    "type": "number",
-					"default": "N/A"
-                }
+            "range": {
+                "description": "The distance in meters from the map (note that most 2-D map renderers do not support infinite zoom and the range will be translated into the nearest supported zoom level).",
+                "type": "number",
+                "default": "N/A"
+            }
         },
         "required": ["range"]
     },
     notes: []
-}
-
-
+};
