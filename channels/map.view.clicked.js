7edac2004d3764ade3bdc049f25e95b10585c0e8
Ismael M Rivera <im.rivera@comcast.net>
2014-09-05 09:22:36 -0400

    Updated keys definition.

--- channels/map.view.clicked.js
+++ channels/map.view.clicked.js
@@ -8,37 +8,37 @@ cmapi.channel["map.view.clicked"] = {
       "lat": {
         "type": "number",
         "description": "The latitude of the location that was clicked",
-		"default": "N/A",
+        "default": "N/A",
         "minimum": "-90",
         "maximum": "90"
       },
       "lon": {
         "type": "number",
         "description": "The longitude of the location that was clicked",
-		"default": "N/A",
+        "default": "N/A",
         "minimum": "-180",
         "maximum": "180"
       },
       "button": {
-        "description": "Which mouse button was clicked.  Allowable values are “right,” “left,” and “middle.”  For backwards compatibility, if this attribute is not populated it MUST be treated as a left mouse click the same as if it were populated with “left.”",
+        "description": "Which mouse button was clicked.  Allowable values are right, left, and middle.  For backwards compatibility, if this attribute is not populated it MUST be treated as a left mouse click the same as if it were populated with left.",
         "type": ["string", "enum"],
-		"enum": ["left", "middle", "right"],
+        "enum": ["left", "middle", "right"],
         "default": "left"
       },
       "type": {
-        "description": "The type of click event. Allowable values are “single” and “double.”  For backwards compatibility, if this attribute is not populated it MUST be assumed to be a single mouse click and treated the same as if it were populated with “single.”",
+        "description": "The type of click event. Allowable values are single and double.  For backwards compatibility, if this attribute is not populated it MUST be assumed to be a single mouse click and treated the same as if it were populated with single.",
         "type": ["string", "enum"],
-		"enum": ["single", "double"],
+        "enum": ["single", "double"],
         "default": "single"
       },
       "keys": {
-        "description": "An array of keys pressed during the click event.  Allowable values are “alt,” “ctrl,” “shift,” and “none.” For backwards compatibility, if this attribute is not populated it MUST be assumed that no additional keys were pressed and behave the same way as if it were populated with “none.”",
-        "type": ["array", "enum"],
+        "description": "An array of keys pressed during the click event.  Allowable values are alt, ctrl, shift, and none. For backwards compatibility, if this attribute is not populated it MUST be assumed that no additional keys were pressed and behave the same way as if it were populated with none.",
+        "type": "array",
         "uniqueItems": true,
         "default": ["none"],
         "items": {
-          "anyOf": ["shift", "alt", "ctrl", "none"]
-            }
+            "enum": ["shift", "alt", "ctrl", "none"]
+        }
       }
     },
     "required": ["lat", "lon", "button", "keys", "type"]
