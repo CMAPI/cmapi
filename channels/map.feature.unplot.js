abfc2236105dd4e870b28d5380e9707866383ed5
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-09-11 14:12:51 -0700

    Minor formatting Added changeLog

--- channels/map.feature.unplot.js
+++ channels/map.feature.unplot.js
@@ -1,28 +1,31 @@
 cmapi.channel["map.feature.unplot"] = {
-    schema: {
-        "$schema": "http://json-schema.org/draft-04/schema#",
-        "title": "map.feature.unplot",
-        "description": "Removes feature data from the map.",
-        "type": "object",
-        "properties": {
-            "overlayId": {
-                "description": "The ID of the overlay where the feature to be removed is found. If no overlayId is included, default overlay with an ID equal to sending widget's ID is assumed.",
-                "type": "string",
-                "default": "sending widget's ID"
-            },
-            "featureId": {
-                "description": "The ID of the feature to be removed.",
-                "type": "string",
-                "default": "N/A"
-            },
-            "messageId": {
-              "description": "A globally unique ID that identifies this particular message batch.  If the messageId property is populated, maps that support the user manipulation extension MUST use this messageId in the map.message.complete, map.message.progress, and map.message.cancel messages as defined in the User Manipulation extension to indicate progress and either completion or cancellation (as appropriate) of the message batch.",
-              "type": "string"
-            }
-        },
-        "required": ["featureId"]
+  "schema": {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "map.feature.unplot",
+    "description": "Removes feature data from the map.",
+    "type": "object",
+    "properties": {
+      "overlayId": {
+        "description": "The ID of the overlay where the feature to be removed is found. If no overlayId is included, default overlay with an ID equal to sending widget's ID is assumed.",
+        "type": "string",
+        "default": "sending widget's ID"
+      },
+      "featureId": {
+        "description": "The ID of the feature to be removed.",
+        "type": "string",
+        "default": "N/A"
+      },
+      "messageId": {
+        "description": "A globally unique ID that identifies this particular message batch.  If the messageId property is populated, maps that support the user manipulation extension MUST use this messageId in the map.message.complete, map.message.progress, and map.message.cancel messages as defined in the User Manipulation extension to indicate progress and either completion or cancellation (as appropriate) of the message batch.",
+        "type": "string",
+        "status": "new"
+      }
     },
-    notes: []
-}
-
-
+    "required": ["featureId"]
+  },
+  "notes": [],
+  "changeLog": [{
+    "version": "1.3.0",
+    "change": "Added optional messageId parameter to support user manipulation extension"
+  }]
+};
