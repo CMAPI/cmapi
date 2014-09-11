ebd5a2859903b2b15ee955ff7d54c3bae2456a82
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-09-11 14:11:24 -0700

    Minor Formatting Added changeLog

--- channels/map.feature.show.js
+++ channels/map.feature.show.js
@@ -1,29 +1,33 @@
 cmapi.channel["map.feature.show"] = {
-    schema: {
-        "$schema": "http://json-schema.org/draft-04/schema#",
-        "title": "map.feature.show",
-        "description": "Have the map show previously hidden feature data.",
-        "type": "object",
-        "properties": {
-                  "overlayId": {
-                    "description": "The ID of the overlay where the feature to be shown is found. If no overlayId is included, default overlay with ID equal to sending widget’s ID is assumed.",
-                    "type": "string",
-					"default": "sending widget's ID"
-                },
-                "featureId": {
-                    "description": "The ID of the feature to be shown.",
-                    "type": "string",
-					"default": "N/A"
-                },
-                "zoom": {
-                    "description": "true if map should zoom to the shown feature, false if not. Default is false.",
-                    "type": "boolean",
-					"default": false
-                }
-        },
-        "required": ["featureId"]
+  "schema": {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "map.feature.show",
+    "description": "Have the map show previously hidden feature data.",
+    "type": "object",
+    "properties": {
+      "overlayId": {
+        "description": "The ID of the overlay where the feature to be shown is found. If no overlayId is included, default overlay with ID equal to sending widget’s ID is assumed.",
+        "type": "string",
+        "default": "sending widget's ID"
+      },
+      "featureId": {
+        "description": "The ID of the feature to be shown.",
+        "type": "string",
+        "default": "N/A"
+      },
+      "zoom": {
+        "description": "true if map should zoom to the shown feature, false if not. Default is false.",
+        "type": "boolean",
+        "default": false
+      },
+      "messageId": {
+        "description": "A globally unique ID that identifies this particular message batch.  If the messageId property is populated, maps that support the user manipulation extension MUST use this messageId in the map.message.complete, map.message.progress, and map.message.cancel messages as defined in the User Manipulation extension to indicate progress and either completion or cancellation (as appropriate) of the message batch.",
+        "type": "string",
+        "status": "new"
+      }
     },
-    notes: []
-}
-
-
+    "required": ["featureId"]
+  },
+  "notes": [],
+  "changeLog": []
+};
