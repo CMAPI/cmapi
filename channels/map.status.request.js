11e6ddd3648093b7574b4834590c81f55675d846
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-09-11 15:26:05 -0700

    Adjusted Schema to use the enumeration of status types correctly Updated changeLog

--- channels/map.status.request.js
+++ channels/map.status.request.js
@@ -8,9 +8,10 @@ cmapi.channel["map.status.request"] = {
       "types": {
         "description": "An array of status types being requested. Map widgets MUST support status types of about, format, selected, view, <span class=\"updatedContent\">and initialization </span>by responding with the applicable status message (e.g., map.status.about, map.status.format, map.status.selected and/or map.status.view). If the types attribute is not included, all status messages MUST be generated.",
         "type": "array",
-        "default": "N/A",
-        "uniqueItems": true,
-        "anyOf": ["view", "about", "format", "selected", "initialization"]
+        "items": {
+          "enum": ["view", "format", "selected", "about", "initialization"]
+        }
+
       },
       "messageId": {
         "description": "A globally unique ID that identifies a particular message.  This ID SHALL be used for the lifetime of the message and is used to identify the response of the request. The map SHALL not issue a map.message.complete messages.",
@@ -24,5 +25,8 @@ cmapi.channel["map.status.request"] = {
   "changeLog": [{
     "version": "1.3.0",
     "change": "Added initialization as a request option"
+  }, {
+    "version": "1.3.0",
+    "change": "Added optional messageId parameter to support user manipulation extension"
   }]
 };
