7f14fb72a80916cb175541dd7e9e3f3e4d81f7c5
Chris Bashioum <cbashioum@mitre.org>
2014-09-29 11:46:17 -0400

    Updated description of the "types" object for clarity

--- channels/map.status.request.js
+++ channels/map.status.request.js
@@ -6,7 +6,7 @@ cmapi.channel["map.status.request"] = {
     "type": "object",
     "properties": {
       "types": {
-        "description": "An array of status types being requested. Map widgets MUST support status types of about, format, selected, view, <span class=\"updatedContent\">and initialization </span>by responding with the applicable status message (e.g., map.status.about, map.status.format, map.status.selected and/or map.status.view). If the types attribute is not included, all status messages MUST be generated.",
+        "description": "An array of status types being requested. Map widgets MUST support status types of 'about', 'format', 'selected', 'view', <span class=\"updatedContent\">and 'initialization' and MUST respond</span> with the applicable status message (e.g., map.status.about, map.status.format, map.status.selected, map.status.view, <span class=\"updatedContent\">and/or map.status.initialization<\span>). If the types attribute is not included, all status messages MUST be generated.",
         "type": "array",
         "items": {
           "enum": ["view", "format", "selected", "about", "initialization"]
