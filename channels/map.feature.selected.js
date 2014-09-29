09d3e7cee9800258c49fb875470970de103a2d8c
Chris Bashioum <cbashioum@mitre.org>
2014-09-29 12:51:00 -0400

    Updated messageId description for clarity

--- channels/map.feature.selected.js
+++ channels/map.feature.selected.js
@@ -25,7 +25,7 @@ cmapi.channel["map.feature.selected"] = {
         "default": "sending widget's ID"
       },
       "messageId": {
-        "description": "A globally unique ID that identifies this particular message batch.  If the messageId property is populated, maps that support the user manipulation extension MUST use this messageId in the map.message.complete, map.message.progress, and map.message.cancel messages as defined in the User Manipulation extension to indicate progress and either completion or cancellation (as appropriate) of the message batch.",
+        "description": "A globally unique ID that identifies this particular message.  If the messageId property is populated, maps that support the user manipulation extension MUST use this messageId in the map.message.complete, map.message.progress, and map.message.cancel messages as defined in the User Manipulation extension to indicate progress and either completion or cancellation (as appropriate) of the message.",
         "type": "string",
         "status": "new",
         "extension": "User Manipulation - Message Complete"
