95385af6cf16a0dce2d9dca5e1d46f42796663bc
chris.bashioum <chris.bashioum@MACEDT-BASHIOUM.snc.dev.lab>
2014-10-21 16:52:29 -0400

    Removed note that this is intended to be an array

--- channels/map.status.selected.js
+++ channels/map.status.selected.js
@@ -42,7 +42,6 @@ cmapi.channel["map.status.selected"] = {
   },
   "notes": [
     "Within a given selectedFeature, although both selectedId and selectedName are optional, one MUST be passed in if a sub-feature is to be identified. Generally, selectedId is preferred and selectedName is used when no selectedId is available. The implication of this is that if sub-features cannot be identified, they cannot be selected.",
-    "The payload sent out in map.selected.status is intended to be itself an array.",
 	"If the list of selected objects spans multiple overlays, then the payload will be an array of messages – one message for each overlay that contains selected objects."
   ],
   "changeLog": []
