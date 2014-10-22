50bcb45ff8d97de4145fc0499d471c91337626ec
chris.bashioum <chris.bashioum@MACEDT-BASHIOUM.snc.dev.lab>
2014-10-22 10:40:42 -0400

    Updated title to make it clear this is a schema definition for an already existing message.complete channel vice a whole new channel

--- channels/map.feature.draw.complete.js
+++ channels/map.feature.draw.complete.js
@@ -1,8 +1,8 @@
 cmapi.channel["map.feature.draw.complete"] = {
   schema: {
     "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "map.feature.draw.complete",
-    "description": "Schema for the details object for a map.message.complete message during a map.feature.draw.",
+    "title": "Schema for map.message.complete Details object",
+    "description": "Schema for the details object for a map.message.complete message after a map.feature.draw.",
     "type": "object",
     "properties": {
       "overlayId": {
