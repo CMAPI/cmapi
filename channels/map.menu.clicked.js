fbfe6f2ca86a6ed108ced549f061353916d234d4
chris.bashioum <chris.bashioum@MACEDT-BASHIOUM.snc.dev.lab>
2014-10-22 14:35:55 -0400

    Cleaned up the descriptions and notes

--- channels/map.menu.clicked.js
+++ channels/map.menu.clicked.js
@@ -5,55 +5,53 @@ cmapi.channel["map.menu.clicked"] = {
     "description": "Called by the map after a context menu item is clicked to notify the widget which registered the context menu to take action.",
     "properties": {
       "menuId": {
-        "description": "The unique ID of the menu.  This ID can be used to associate a menu with a feature in a map.feature.plot message.",
+        "description": "The unique ID of the menu that the user selected.  MUST be the menuId that was assigned when the menu was registered.",
         "type": "string"
       },
       "menuItemId": {
         "type": "string",
-        "description": "Unique ID used to correlate a map.menu.clicked message with this particluar menu item.  This ID MUST only be unique within this menu"
+        "description": "The unique ID of the specific menu item that the user selected.  MUST be the menuItemId that was assigned when the menu was registered."
       },
       "overlayId": {
-        "description": "The ID of the overlay the clicked feature is on if the menu was a feature context menu or an overlay context menu",
+        "description": "The ID of the overlay, or the overlay containing the feature, that was right-clicked to bring up the context menu.  SHOULD be populated if the context menu was an 'objectinstance' scoped context menu",
         "type": "string",
-        "default": "sending widget's ID"
+        "default": "N/A"
       },
       "featureId": {
-        "description": "The ID of the clicked feature if the menu is a feature menu",
+        "description": "The ID of the feature that was right-clicked to bring up the context menu.  SHOULD be populated if the context menu was an 'objectinstance' scoped context menu",
         "type": "string"
       },
       "lat": {
-        "description": "The latitude coordinate is degrees decimal of where the click took place on the map",
+        "description": "The latitude coordinate in degrees decimal identifying where the right-click took place on the map",
         "type": "number",
         "minimum": -90,
         "maximum": 90
       },
       "lon": {
-        "description": "The longitude coordinate is degrees decimal of where the click took place on the map",
+        "description": "The longitude coordinate in degrees decimal of where the right-click took place on the map",
         "type": "number",
         "minimum": -180,
         "maximum": 180
       },
       "x": {
-        "description": "The pixel coordinate on the x axis relative to the top left corner of the map",
+        "description": "The pixel coordinate on the x axis where the right-click took place on the map, relative to the top left corner of the map",
         "type": "number",
         "minimum": 0
       },
       "y": {
-        "description": "The pixel coordinate on the y axis relative to the top left corner of the map",
+        "description": "The pixel coordinate on the y axis where the right-click took place on the map, relative to the top left corner of the map",
         "type": "number",
         "minimum": 0
       },
       "elevation": {
-        "description": "The elevation of the location clicked in meters if terrain elevation data is available",
+        "description": "The elevation in meters of the location that was right-clicked, if terrain elevation data is available",
         "type": "number"
       }
 
     },
     "required": ["menuId", "menuItemId"]
   },
-  "notes": ["When the user right clicks on a map feature that has a menu registered with it, or the feature listing in a tree type component, , the registered context menu and with the applicable context items appear.",
-    "When the user right clicks on the map and it has a menu registered with it, the registered context menu and with the applicable context items appear.",
-    "When user selects a menu item, an event is sent back to the widget that created the context menu and can take action on the event"
+  "notes": ["When the user selects a context menu item, the map MUST send a map.menu.clicked event with the appropriately registered menuId and menuItemId so that the widget that created the context menu can take action on the event"
   ],
   "changeLog": [{
     "version": "1.3.0",
