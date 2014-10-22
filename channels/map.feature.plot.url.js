a87a312cd13972464f80316743085ae1f515912d
chris.bashioum <chris.bashioum@MACEDT-BASHIOUM.snc.dev.lab>
2014-10-22 12:39:29 -0400

    Added menuId to the params to support context menus

--- channels/map.feature.plot.url.js
+++ channels/map.feature.plot.url.js
@@ -52,6 +52,12 @@ cmapi.channel["map.feature.plot.url"] = {
         "type": "string",
         "status": "new",
         "extension": "User Manipulation - Message Complete"
+      },
+	  "menuId": {
+        "description": "The id of a context menu. If populated, the context menu MUST have already been pre-registered via the map.menu.create channel. If populated, the context menu associated with this id will appear when the feature is 'right-clicked', allowing the user to invoke actions on the feature which will be handled by the widget which originally registered the context menu. If no menuId is assigned, the feature will not have a context menu associated when right-clicked.",
+        "type": "string",
+        "status": "new",
+        "extension": "User Manipulation - Context Menus"
       }
     },
     "required": ["featureId", "url"]
@@ -62,5 +68,8 @@ cmapi.channel["map.feature.plot.url"] = {
   "changeLog": [{
     "version": "1.3.0",
     "change": "Added optional messageId parameter to support user manipulation extension"
+  }, {
+    "version": "1.3.0",
+    "change": "Added optional menuId parameter to support user manipulation extension"
   }] 
 };
