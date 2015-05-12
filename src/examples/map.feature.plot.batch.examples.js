fdec83aeac1ad6a22e1f7fc30caddd6e1568f88c
Chris Bashioum <chris.bashioum@gmail.com>
2015-05-12 14:15:56 -0400

    Added map.feature.plot.batch example file

---
+++ src/examples/map.feature.plot.batch.examples.js
@@ -0,0 +1,27 @@
+cmapi.channel["map.feature.plot.batch"].examples = [{
+  "title": "Plot KML batch",
+  "description": "",
+  "valid": true,
+  "payload": {"features": [
+	{
+		"featureId": "example.mapWidget.1",
+		"feature": "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\"><Placemark id=\"example.mapWidget.1.1\"><name>World Trade Center</name><description><![CDATA[Site of World Trade Center]]></description><Style><IconStyle><Icon><href>https://localhost/widgets/mapWidget/images/blu-circle.png</href></Icon><hotSpot x=\"0.5\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"></hotSpot></IconStyle></Style><Point><coordinates>-74.01324033737183,40.71149172571141,0 </coordinates></Point></Placemark></kml>",
+		"name": "World Trade Center"
+	},{
+		"featureId": "example.mapWidget.2",
+		"feature": "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\"><Placemark id=\"example.mapWidget.1.2\"><name>911 Memorial Museum</name><description><![CDATA[Site of 911 Memorial Museum]]></description><Style><IconStyle><Icon><href>https://localhost/widgets/mapWidget/images/blu-circle.png</href></Icon><hotSpot x=\"0.5\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"></hotSpot></IconStyle></Style><Point><coordinates>-74.0136,40.7117,0 </coordinates></Point></Placemark></kml>",
+		"name": "World Trade Center"
+	},{
+		"featureId": "example.mapWidget.3",
+		"feature": "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\"><Placemark id=\"example.mapWidget.1.3\"><name>Millenium Hilton</name><description><![CDATA[Site of Millenium Hilton]]></description><Style><IconStyle><Icon><href>https://localhost/widgets/mapWidget/images/blu-circle.png</href></Icon><hotSpot x=\"0.5\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"></hotSpot></IconStyle></Style><Point><coordinates>-74.0103,40.7111,0 </coordinates></Point></Placemark></kml>",
+		"name": "World Trade Center"
+	},{
+		"featureId": "example.mapWidget.4",
+		"feature": "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\"><Placemark id=\"example.mapWidget.1.4\"><name>911 Tribute Center</name><description><![CDATA[Site of 911 Tribute Center]]></description><Style><IconStyle><Icon><href>https://localhost/widgets/mapWidget/images/blu-circle.png</href></Icon><hotSpot x=\"0.5\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"></hotSpot></IconStyle></Style><Point><coordinates>-74.0124,40.7101,0 </coordinates></Point></Placemark></kml>",
+		"name": "World Trade Center"
+	}],
+  "overlayId": "2d882141-0d9e-59d4-20bb-58e6d0460699.1",
+  "format":"kml",
+  "zoom":true,
+  "readOnly":false
+}}];
