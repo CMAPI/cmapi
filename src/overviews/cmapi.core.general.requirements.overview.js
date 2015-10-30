78611c16c43a4f21312ae6577ca6beb426a54b6b
Eric Briscoe <eric.j.briscoe@gmail.com>
2015-10-30 13:38:44 -0700

    Addresses issue #43 to clarify when to use lower case, camel case when documenting the Object keywords and values

--- src/overviews/cmapi.core.general.requirements.overview.js
+++ src/overviews/cmapi.core.general.requirements.overview.js
@@ -40,7 +40,8 @@ cmapi.overview["cmapi.core.general.requirements.overview"] = {
   }, {
     "title": "Case Sensitivity",
     "paragraphs": [
-      "All keywords (i.e., those specifically identified in this API) in a message payload are case sensitive unless otherwise noted"
+      "All keywords (i.e., those specifically identified in this API) in a message payload are case sensitive unless otherwise noted.",
+      "Keywords and object property values SHOULD be in lower case or when they are multiple words SHOULD be be in camel case"
     ]
   }, {
     "title": "Coordinate Reference System",
