c5ca84c9dc33746f6701b2b933c33a5466d3bbe0
Eric Briscoe <eric.j.briscoe@gmail.com>
2016-08-16 15:18:52 -0700

    Fixes Github issue #66 where the documentation for the map.message.complete for a map.get was incorrect

--- src/descriptions/map.get.complete.description.js
+++ src/descriptions/map.get.complete.description.js
@@ -1,13 +1,16 @@
 cmapi.channel["map.get.complete"].description = {
-  "description": "Allows the Map Widget to report progress during the processing of a message.  If a widget receives a map.message.progress message but does not have a record of sending a message with the matching messageId, then the widget should ignore the message.  Note that not all channels that support a messageId will report progress between when the message is sent and the return of the map.message.complete.  See each channel’s definition for map.message.progress details object so see if the channel supports progress messages.  Multiple map.message.progress events may be sent for a single message. E.g., every time a new point is added to a line during the processing of a map.feature.draw message, a map.message.progress message will be sent with the latest geometry of the line being drawn as shown in Example 1 below.",
+  "description": "Schema for the details object for a map.message.complete message after a map.get",
   "properties": {
-    "messageId": {
-      "description": "A globally unique ID that identifies the particular message or message batch that progress is being reported on.",
-      "defaultValue": ""
-    },
-    "details": {
-      "description": "An object whose details are specific to the original requesting channel/message.  Go to the specific channel definition for details of what this object should look like",
-      "defaultValue": ""
-    }
+      "successes": {
+        "description": "An object containing successful results (features and overlays) pertaining to the filter parameters provided in the corresponding map.get message",
+        "properties" : {
+          "overlay":{
+            "description" : "An array of overlay objects (see map.overlay.create) that meet the map.get fillter criteria"
+          },
+          "feature":{
+            "description" : "An array of feature objects (see map.feature.plot) that meet the map.get fillter criteria"
+          }
+        }
+      }
   }
 };
