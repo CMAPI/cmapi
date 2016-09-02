2d77deb9908a33db4dbbbc71546459512bc42bfe
Eric Briscoe <eric.j.briscoe@gmail.com>
2016-09-02 11:05:47 -0700

    Fix for Github issue #95 for "aded" to "added" typo

--- src/overviews/map.message.overview.js
+++ src/overviews/map.message.overview.js
@@ -3,7 +3,7 @@ cmapi.overview["map.message.overview"] = {
   "sections": [{
     "title": "Overview",
     "paragraphs": [
-      "Namespace for map.message channels that enable feedback for message progress and completion for messsages sent via other channels. The map.message channels require that an attribute messageId be aded to the message payload for any message you wish to get a message complete or message progress event.  The messageId MUST be a unique id which will be used for the duration of a single message.  The channels that support message complete and message progress are listed under those channels."
+      "Namespace for map.message channels that enable feedback for message progress and completion for messsages sent via other channels. The map.message channels require that an attribute messageId be added to the message payload for any message you wish to get a message complete or message progress event.  The messageId MUST be a unique id which will be used for the duration of a single message.  The channels that support message complete and message progress are listed under those channels."
     ]
   }]
 };
